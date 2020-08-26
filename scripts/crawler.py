62db988e2af9580c44a87a58f0951725a2a7c8ae
Artem Kurylev <artembonapart@gmail.com>
2020-08-26 10:22:40 +0300

    Functionality of Searches per sources moved to distinct classes, Added possibility for searching on multiple pages

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -1,14 +1,22 @@
-import requests
-import sys
 import argparse
-from bs4 import BeautifulSoup as bs
+from sources_classes import Springer, Scholar, ACM
 
 SEARCH_TITLE_CLASSES = {'acm': 'issue-item__title', 'scholar': 'gs_rt', 'springer': 'title'}
 SEARCH_ABSTRACT_CLASSES = {'acm': 'issue-item__title'}
 SEARCH_LINKS = {'acm': 'https://dl.acm.org/action/doSearch?AllField=',
                 'scholar': 'https://scholar.google.com/scholar?as_ylo=2016&q=',
-                'springer': 'https://link.springer.com/search?query='}
+                'springer': 'https://link.springer.com/search/page/1?query='}
 additional_args = {'acm': {'features': "lxml"}, 'scholar':  {'features': "lxml"}, 'springer': {'features': "lxml"}}
+
+search_sources = {'springer': Springer, 'scholar': Scholar, 'acm': ACM}
+
+
+class Searcher:
+    def __init__(self, source, filename, query, n_pages=1):
+        self.source = search_sources[source](filename, query, n_pages)
+
+    def search(self):
+        return self.source.search()
 
 
 def save_text_file(filename, text_lines):
@@ -18,49 +26,12 @@ def save_text_file(filename, text_lines):
         wf.close()
 
 
-def search_scholar_page(html_page, search_class):
-    title_classes = html_page.findAll(class_=search_class)
-    titles = []
-    for title_object in title_classes:
-        title = title_object.find('a')
-        titles.append(title)
-    return titles
-
-
-def search_springer_page(html_page, search_class):
-    title_classes = html_page.findAll(class_=search_class)
-    titles = []
-    for title_object in title_classes:
-        titles.append(title_object)
-    return titles
-
-
-def main(source, query, filename='default.txt', abstract=False):
+def main(source, query, filename='default.txt', abstract=False, n_pages=1):
     query_string = ''
     for word in query:
         query_string += word + '+'
-    search_class = SEARCH_TITLE_CLASSES[source]
-    search_link = SEARCH_LINKS[source]
-
-    query_string = query_string[:-1]
-    request_addr = search_link + query_string
-    page = requests.get(request_addr)
-    html_page = bs(page.text, **additional_args[source])
-
-    if source == 'scholar':
-        title_classes = search_scholar_page(html_page, search_class)
-        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.get("href")} \n\n' for idx, title in
-                  enumerate(title_classes)]
-    elif source == 'springer':
-        title_classes = search_springer_page(html_page, search_class)
-        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.get("href")} \n\n' for idx, title in
-                  enumerate(title_classes)]
-    else:
-        title_classes = html_page.findAll(class_=search_class)
-
-        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.find("a").get("href")} \n\n' for idx, title in
-                  enumerate(title_classes)]
-
+    searcher = Searcher(source, filename, query, n_pages)
+    titles = searcher.search()
     # Get titles from the first page
 
     save_text_file(filename, titles)
@@ -71,6 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--source', dest='source', default='acm', type=str)
     parser.add_argument('--query', dest='query', type=str, nargs='+')
     parser.add_argument('--filename', dest='filename', default='default.txt')
+    parser.add_argument('--npages', dest='n_pages', default=False, type=int)
     parser.add_argument('--abstract', dest='abstract', default=False, type=bool)
     args = parser.parse_args()
-    main(args.source, args.query, args.filename, args.abstract)
+    main(args.source, args.query, args.filename, args.abstract, args.n_pages)
---
+++ scripts/sources_classes.py
@@ -0,0 +1,105 @@
+from bs4 import BeautifulSoup as bs
+from abc import ABC, abstractmethod
+import requests
+
+SEARCH_TITLE_CLASSES = {'acm': 'issue-item__title', 'scholar': 'gs_rt', 'springer': 'title'}
+SEARCH_ABSTRACT_CLASSES = {'acm': 'issue-item__title'}
+SEARCH_LINKS = {'acm': 'https://dl.acm.org/action/doSearch?AllField=',
+                'scholar': 'https://scholar.google.com/scholar?as_ylo=2016&q=',
+                'springer': 'https://link.springer.com/search/page/1?query='}
+additional_args = {'acm': {'features': "lxml"}, 'scholar':  {'features': "lxml"}, 'springer': {'features': "lxml"}}
+
+
+class Source(ABC):
+    def __init__(self, filename, query, n_pages, source_type):
+        self.filename = filename
+        self.query = ''
+        for word in query:
+            self.query += word + '+'
+        self.n_pages = n_pages
+        self.search_link = SEARCH_LINKS[source_type] + self.query
+        self.search_class = SEARCH_TITLE_CLASSES[source_type]
+
+    @abstractmethod
+    def search(self):
+        return []
+
+
+class Scholar(Source):
+    def __init__(self, filename, query, n_pages=1):
+        super(Scholar, self).__init__(filename, query, n_pages, 'scholar')
+
+    def search(self):
+        title_classes = []
+        for i in range(self.n_pages):
+            start = i * 10 if i > 0 else 0
+            if i > 0:
+                self.search_link += f'&start={start}'
+            page = requests.get(self.search_link)
+            html_page = bs(page.text, **additional_args['scholar'])
+            title_classes += Scholar.search_scholar_page(html_page, self.search_class)
+        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.get("href")} \n\n' for idx, title in
+                  enumerate(title_classes)]
+        return titles
+
+    @staticmethod
+    def search_scholar_page(html_page, search_class):
+        title_classes = html_page.findAll(class_=search_class)
+        titles = []
+        for title_object in title_classes:
+            title = title_object.find('a')
+            titles.append(title)
+
+        return titles
+
+
+class Springer(Source):
+    def __init__(self, filename, query, n_pages=1):
+        super(Springer, self).__init__(filename, query, n_pages, 'springer')
+
+    def search(self):
+        title_objects = []
+        for i in range(self.n_pages):
+            page_num = i + 1
+            if i > 1:
+                self.search_link.replace('1', str(page_num))
+            page = requests.get(self.search_link)
+            html_page = bs(page.text, **additional_args['springer'])
+            title_objects += Springer.search_springer_page(html_page, self.search_class)
+        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.get("href")} \n\n' for idx, title in
+                  enumerate(title_objects)]
+        return titles
+
+    @staticmethod
+    def search_springer_page(html_page, search_class):
+        title_classes = html_page.findAll(class_=search_class)
+        titles = []
+        for title_object in title_classes:
+            titles.append(title_object)
+        return titles
+
+
+class ACM(Source):
+    def __init__(self, filename, query, n_pages=1):
+        super(ACM, self).__init__(filename, query, n_pages, 'acm')
+
+    def search(self):
+        title_classes = []
+        for i in range(self.n_pages):
+            startPage = i
+            if i > 0:
+                self.search_link += f'startPage={startPage}'
+            page = requests.get(self.search_link)
+            html_page = bs(page.text, **additional_args['acm'])
+            title_classes += ACM.search_acm_page(html_page, self.search_class)
+        titles = [f'{idx + 1}) Name: {title.text} \n\n Link: {title.find("a").get("href")} \n\n' for idx, title in
+                  enumerate(title_classes)]
+        return titles
+
+    @staticmethod
+    def search_acm_page(html_page, search_class):
+        title_classes = html_page.findAll(class_=search_class)
+        titles = []
+        for title_object in title_classes:
+            titles.append(title_object)
+        return titles
